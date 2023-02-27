9570103ac81fc3b68a603ce0711d8c7bd8b27179
Naomi Phillips <naomi@nep.soy>
2023-02-27 15:19:02 -0500

    Add emptyish /channel POST endpoint

--- src/web/api.rs
+++ src/web/api.rs
@@ -2,9 +2,33 @@ use actix_web::{get, post, web, Responder};
 
 use super::req::*;
 use super::resp::*;
+use super::{State, UrlState};
 
 #[post("/channel")]
-pub async fn create_channel(request: web::Json<CreateChannelReq>) -> impl Responder {
-    info!("Creating channel with remote {}", request.remote.as_str());
-    web::Json(CreateChannelResp::Initiated)
+pub async fn create_channel(data: web::Data<State>, request: web::Json<CreateChannelReq>) -> impl Responder {
+    debug!("POST /channel ({})", request.remote.as_str());
+    let url_map = data.url_map.clone();
+    let mut guard = url_map.lock().await;
+    match guard.get(&request.remote) {
+        None => {
+            guard.insert(request.remote.clone(), UrlState::InProgress);
+            tokio::spawn(async move {
+                let url_map = data.url_map.clone();
+                info!("Adding {} ...", &request.remote);
+                tokio::time::sleep(tokio::time::Duration::from_secs(3)).await;
+                let result_state = UrlState::Success { slug: "placeholder".to_string() };
+                url_map.lock().await.insert(request.remote.clone(), result_state);
+            });
+            web::Json(CreateChannelResp::Initiated)
+        },
+        Some(UrlState::InProgress) => {
+            web::Json(CreateChannelResp::InProgress)
+        },
+        Some(UrlState::Success { slug }) => {
+            web::Json(CreateChannelResp::Success { slug: slug.clone() })
+        },
+        Some(UrlState::Failure { error }) => {
+            web::Json(CreateChannelResp::Failure { error: error.clone() })
+        },
+    }
 }
--- src/web/mod.rs
+++ src/web/mod.rs
@@ -2,11 +2,37 @@ mod api;
 mod req;
 mod resp;
 
-use actix_web::{HttpServer, App};
+use std::{sync::Arc, collections::HashMap};
+
+use actix_web::{HttpServer, App, web};
+use tokio::sync::Mutex;
+use url::Url;
+
+#[derive(Clone, Debug)]
+pub enum UrlState {
+    InProgress,
+    Success { slug: String },
+    Failure { error: String },
+}
+
+#[derive(Clone)]
+pub struct State {
+    url_map: Arc<Mutex<HashMap<Url, UrlState>>>,
+}
+
+impl State {
+    pub fn new() -> Self {
+        Self {
+            url_map: Arc::new(Mutex::new(HashMap::new())),
+        }
+    }
+}
 
 pub async fn run(host: impl ToString, port: u16) -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let state = State::new();
+    HttpServer::new(move || {
         App::new()
+            .app_data(web::Data::new(state.clone()))
             .service(api::create_channel)
     })
     .bind((host.to_string(), port))?
